d06646843890bf5ca67204fc77c7146194ca7505
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-03-14 18:32:23 +0000

    Add the scripts to download and extract archives.

---
+++ scripts/extract.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+function untar() {
+
+  tar_archive=$1
+  destination=$2
+
+  if [ ! -d ${destination} ]; then
+    inner_name=$(tar -tf "${tar_archive}" | grep -o '^[^/]\+' | sort -u)
+    ctx logger info "Untaring ${tar_archive}"
+    tar -zxvf ${tar_archive}
+
+    ctx logger info "Moving ${inner_name} to ${destination}"
+    mv ${inner_name} ${destination}
+  fi
+}
+
+function unzip() {
+
+  zip_archive=$1
+  destination=$2
+
+  if [ ! -d ${destination} ]; then
+    if [[ ${zip_archive} == *".zip"* ]]; then
+
+      set +e
+      unzip_cmd=$(which unzip)
+      set -e
+
+      if [[ -z ${unzip_cmd} ]]; then
+        ctx logger error "Cannot extract ${zip_archive}: 'unzip' command not found."
+        exit 1
+      fi
+
+      inner_name=$(unzip -qql "${zip_archive}" | sed -r '1 {s/([ ]+[^ ]+){3}\s+//;q}')
+      ctx logger info "Unzipping ${zip_archive}"
+      unzip ${zip_archive}
+
+      ctx logger info "Moving ${inner_name} to ${destination}"
+      mv ${inner_name} ${destination}
+
+    else
+
+      # assuming tarball if the archive is not a zip.
+      # we dont check that tar exists since if we made it
+      # this far, it definitely exists (nodejs used it)
+      untar(tar_archive, destination)
+}
