a1751ba917385301b884b8b3cb73b4a36d968e13
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-03-16 18:34:43 +0000

    Remove the scripts to download and extract and add the functions responsible to do that to each script.

--- scripts/mysql/install-mysql.sh
+++ scripts/mysql/install-mysql.sh
@@ -1,12 +1,54 @@
-#!bin/bash
-set -e
+#!/bin/bash -e
 
-MYSQL_DIR='/usr/local/'
+function download() {
+
+  url=$1
+  name=$2
+
+  if [ -f "`pwd`/${name}" ]; then
+    echo "`pwd`/${name} already exists, no need to download"
+  else
+    # download the given directory
+    echo "Dowloading ${url} to `pwd`/${name}"
+
+    set +e
+    curl_cmd=$(which curl)
+    wget_cmd=$(which wget)
+    set -e
+
+    if [[ ! -z ${curl_cmd} ]]; then
+      curl -L -o ${name} ${url}
+    elif [[ ! -z ${wget_cmd} ]]; then
+      wget -O ${name} ${url}
+    else
+      echo "Failed to download ${url}: Neither 'cURL' nor 'wget' were found on the system"
+      exit 1;
+    fi
+  fi
+}
+
+function untar() {
+
+  tar_archive=$1
+  destination=$2
+
+  if [ ! -d ${destination} ]; then
+    inner_name=$(tar -tf "${tar_archive}" | grep -o '^[^/]\+' | sort -u)
+    echo "Untaring ${tar_archive}"
+    tar -zxvf ${tar_archive}
+
+    echo "Moving ${inner_name} to ${destination}"
+    mv ${inner_name} ${destination}
+  fi
+}
+
+TMP_DIR='/tmp'
+MYSQL_DIR='/usr/local/mysql'
 MYSQL_MIRROR=''
 MYSQL_TARBALL='mysql-server_5.6.23-1ubuntu14.10_i386.deb-bundle.tar'
 
-cd ${MYSQL_DIR}
-./download.sh ${MYSQL_MIRROR}/${MYSQL_TARBALL} ${MYSQL_TARBALL}
-./untar.sh ${MYSQL_TARBALL} ${MYSQL_DIR}
+cd ${TMP_DIR}
+download ${MYSQL_MIRROR}/${MYSQL_TARBALL} ${MYSQL_TARBALL}
+untar ${MYSQL_TARBALL} ${MYSQL_DIR}
 
-ctx logger info 'MySQL installed successfuly.'
+echo 'MySQL installed successfuly.'
--- scripts/tomcat/install-tomcat.sh
+++ scripts/tomcat/install-tomcat.sh
@@ -1,15 +1,60 @@
-#!bin/bash
-set -e
+#!/bin/bash -e
 
-TOMCAT_DIR='/usr/local/'
+function download() {
+
+  url=$1
+  name=$2
+
+  if [ -f "`pwd`/${name}" ]; then
+    echo "`pwd`/${name} already exists, no need to download"
+  else
+    # download the given directory
+    echo "Dowloading ${url} to `pwd`/${name}"
+
+    set +e
+    curl_cmd=$(which curl)
+    wget_cmd=$(which wget)
+    set -e
+
+    if [[ ! -z ${curl_cmd} ]]; then
+      curl -L -o ${name} ${url}
+    elif [[ ! -z ${wget_cmd} ]]; then
+      wget -O ${name} ${url}
+    else
+      echo "Failed to download ${url}: Neither 'cURL' nor 'wget' were found on the system"
+      exit 1;
+    fi
+  fi
+}
+
+function untar() {
+
+  tar_archive=$1
+  destination=$2
+
+  if [ ! -d ${destination} ]; then
+    inner_name=$(tar -tf "${tar_archive}" | grep -o '^[^/]\+' | sort -u)
+    echo "Untaring ${tar_archive}"
+    tar -zxvf ${tar_archive}
+
+    echo "Moving ${inner_name} to ${destination}"
+    mv ${inner_name} ${destination}
+  else
+    echo "Could not extract."
+  fi
+}
+
+
+TMP_DIR='/tmp'
+TOMCAT_DIR='/tmp/tomcat'
 TOMCAT_MIRROR='http://mirrors.ibiblio.org/apache/tomcat/tomcat-7/v7.0.59/bin'
 TOMCAT_TARBALL='apache-tomcat-7.0.59.tar.gz'
 TOMCAT_TARBALL_NAME='apache-tomcat-7.0.59'
 
-cd ${TOMCAT_DIR}
-./download.sh ${TOMCAT_MIRROR}/${TOMCAT_TARBALL} ${TOMCAT_TARBALL}
-./untar.sh ${TOMCAT_TARBALL} ${TOMCAT_DIR}
+cd ${TMP_DIR}
+#download ${TOMCAT_MIRROR}/${TOMCAT_TARBALL} ${TOMCAT_TARBALL}
+untar ${TOMCAT_TARBALL} ${TOMCAT_DIR}
 
 export CATALINA_HOME=${TOMCAT_DIR}/${TOMCAT_TARBALL_NAME}
 
-ctx logger info 'Tomcat installed successfuly.'
+echo 'Tomcat installed successfuly.'
