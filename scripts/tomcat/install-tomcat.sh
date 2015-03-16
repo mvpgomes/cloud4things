3ea79a7823938195a708d87434cf7d2dd13adffb
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-03-16 16:24:00 +0000

    Add the installation scripts for EPCIS, Tomcat and MySQl.

--- scripts/tomcat/install-tomcat.sh
+++ scripts/tomcat/install-tomcat.sh
@@ -1,8 +1,14 @@
 #!bin/bash
 
 TOMCAT_DIR='/usr/local/'
-TOMCAT_MIRROR=''
+TOMCAT_MIRROR='http://mirrors.ibiblio.org/apache/tomcat/tomcat-7/v7.0.59/bin'
 TOMCAT_TARBALL='apache-tomcat-7.0.59.tar.gz'
+TOMCAT_TARBALL_NAME='apache-tomcat-7.0.59'
 
 cd ${TOMCAT_DIR}
-../
+../download.sh ${TOMCAT_MIRROR}/${TOMCAT_TARBALL} ${TOMCAT_TARBALL}
+../untar.sh ${TOMCAT_TARBALL} ${TOMCAT_DIR}
+
+export CATALINA_HOME=${TOMCAT_DIR}/${TOMCAT_TARBALL_NAME}
+
+ctx logger info 'Tomcat installed successfuly.'
