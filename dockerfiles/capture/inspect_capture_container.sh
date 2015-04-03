a4464325c7ed22b1cff4fc751cd7f06187966b1f
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-04-03 14:12:21 +0100

    Add the scrpits to manage the Capture Docker container closes #45.

---
+++ dockerfiles/capture/inspect_capture_container.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+CONTAINER_NAME='fosstrak_capture'
+
+docker exec -it ${CONTAINER_NAME} bash
---
+++ dockerfiles/capture/stop_capture_container.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+CONTAINER_NAME='fosstrak_capture'
+
+docker stop ${CONTAINER_NAME}
