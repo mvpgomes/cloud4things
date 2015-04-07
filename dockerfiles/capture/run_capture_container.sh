c9ac1f69b7de87e2f5e59df4a01d95730848f6ec
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-04-07 11:14:21 +0100

    Change the container port of the Capture container.

--- dockerfiles/capture/run_capture_container.sh
+++ dockerfiles/capture/run_capture_container.sh
@@ -2,4 +2,4 @@
 
 CONTAINER_NAME='fosstrak_capture'
 
-docker run -it --name ${CONTAINER_NAME} --link fosstrak_epcis:epcisrepo -p 8081:8081 -d mvpgomes/fosstrak_capture
+docker run -it --name ${CONTAINER_NAME} --link fosstrak_epcis:epcisrepo -p 8081:8080 -d mvpgomes/fosstrak_capture
