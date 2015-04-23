aad177e70f45516e803957c43ccd73282e5d4db0
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-04-23 10:07:28 +0100

    Add a chef recipe to provisoning the Fosstrak Capture app container close #65.

---
+++ chef-repo/cookbooks/docker/recipes/fosstrak_capture.rb
@@ -0,0 +1,10 @@
+# Pull latest image
+docker_image 'cloud4things/fosstrak_capture'
+
+# Run container exposing port 9999
+docker_container 'cloud4things/fosstrak_capture' do
+  detach true
+  container_name 'fosstrak_capture'
+  link 'fosstrak_epcis:epcis'
+  port '9999:9999'
+end
