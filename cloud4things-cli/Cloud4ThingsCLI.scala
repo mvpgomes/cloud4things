e692f79c447c99c9c776dce8281a745773201ab3
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-04-29 20:03:37 +0100

    Update the command-line application to allow the users to create readers and event cycles.

--- cloud4things-cli/Cloud4ThingsCLI.scala
+++ cloud4things-cli/Cloud4ThingsCLI.scala
@@ -11,7 +11,9 @@
     - Configure the event-cycles that are associated with
       each reader.
    ------------------------------------------------------- */
+import scala.io.StdIn
 import scala.io.Source
+import scala.collection.mutable.ListBuffer
 
 object Cloud4ThingsCLI {
 
@@ -19,38 +21,79 @@ object Cloud4ThingsCLI {
   var ALEServiceURL : String = _
   var ALELRServiceURL : String = _
 
-  val listOptions = List("1 - Set ALELRService endpoint", "2 - Set ALEService endpoint",
-                        "3 - Define Reader", "4 - Define Event-Cycle", "5 - Help")
+  // List that contains the options available for the user
+  val listOptions = List("1 - Set ALELRService endpoint",
+                         "2 - Set ALEService endpoint",
+                         "3 - Define Reader",
+                         "4 - Define Event-Cycle",
+                         "5 - Help",
+                         "6 - Show Readers",
+                         "7 - Show EventCycles")
 
+  // List of Readers
+  val readers = new ListBuffer[Reader]()
+
+  // List of EventCycles
+  val eventCycles = new ListBuffer[EventCycle]()
+
+  // Print the options for the user choose
   def printOptions() {
     listOptions.foreach(println)
     print("Cloud4ThingsCLI:> ")
   }
 
+  /* A dictionary-based function that invokes a function based
+     on the input entered by the user.*/
   def selectOption(option: String) = option match {
     case "1" => setALELRServiceURL()
     case "2" => setALEServiceURL()
     case "3" => defineReader()
     case "4" => defineEventCycle()
     case "5" => println("Need help!\n")
+    case "6" => showReaders()
+    case "7" => showEventCycles()
     case _ => println("ERROR: Invalid option, choose another one.\n")
   }
 
-  def setALELRServiceURL() { ALELRServiceURL = url }
-
-  def setALEServiceURL() { ALEServiceURL = url }
-
-  def defineReader() : Boolean = {
-    true
+  def setALELRServiceURL() {
+    print("Enter the ALELRService URL: ")
+    ALELRServiceURL = StdIn.readLine()
   }
 
-  def defineEventCycle() : Boolean = {
-    true
+  def setALEServiceURL() {
+    print("Enter the ALEService URL: ")
+    ALEServiceURL = StdIn.readLine()
   }
 
-  def subscribeEventCycle(name: String, url: String) : Boolean = {
-    true
+  def defineReader() {
+    print("Enter the reader name: ")
+    readers += new Reader(StdIn.readLine())
   }
+
+  private def deleteReader(name: String) {
+
+  }
+
+  def defineEventCycle() {
+    print("Enter the event cycle name: ")
+    eventCycles += new EventCycle(StdIn.readLine())
+  }
+
+  private def deleteEventCycle(name: String) {
+
+  }
+
+  def showReaders() {
+    println("Printing readers ....")
+    readers.toList.foreach(println)
+  }
+
+  def showEventCycles() {
+    println("Printing event cycles ....")
+    eventCycles.toList.foreach(println)
+  }
+
+  def subscribeEventCycle(name: String, url: String) {}
 
   def main(args: Array[String]): Unit = {
     printOptions()
