da246f6f10a49130307e0c68be0de9c856904b86
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-04-30 10:33:46 +0100

    Add the attributes required by the configuration file to the class EventCycle.

--- cloud4things-cli/EventCycle.scala
+++ cloud4things-cli/EventCycle.scala
@@ -1,5 +1,13 @@
+/* EventCycle: class that represents a EventCycle that occurs in the smart place.
+   Params:
+    - @eventName : String = the EventCycle name.
+    - @repeatPeriod : Int = the period of a EventCycle in ms.
+    - @duration : Int = the duration of an EventCycle in ms.
+    - @stableSetInterval : Int = the stableSetInterval of the EventCycle.
+*/
 class EventCycle(name: String) {
   val eventName : String = name
+  val associatedReader : String = "Reader"
   val repeatPeriod : Int = 2000
   val duration : Int = 2000
   val stableSetInterval : Int = 0
