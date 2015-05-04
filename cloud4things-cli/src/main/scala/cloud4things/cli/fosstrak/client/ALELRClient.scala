9cb57543b6126e5eb5dadad747a715badbe8bc6d
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-05-04 20:08:30 +0100

    Add the skeleton of the ALELR client.

---
+++ cloud4things-cli/src/main/scala/cloud4things/cli/fosstrak/client/ALELRClient.scala
@@ -0,0 +1,10 @@
+package cloud4things.cli.fosstrak.client
+
+/*
+ * ALELRClient : A ALELR client implementation that
+ * contains the methods stub's for the ALELR service
+ * of the Fosstrak Filtering & Collecting Server.
+ */
+class ALELRClient {
+
+}
