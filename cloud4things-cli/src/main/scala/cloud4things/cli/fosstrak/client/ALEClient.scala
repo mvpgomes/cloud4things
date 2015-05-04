fb8bb4546f2691a0ea0cf7fb68a1be0eaaa44886
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-05-04 20:07:55 +0100

    Add the skeleton of the ALEClient.

---
+++ cloud4things-cli/src/main/scala/cloud4things/cli/fosstrak/client/ALEClient.scala
@@ -0,0 +1,10 @@
+package cloud4things.cli.fosstrak.client
+
+/*
+ * ALEClient : A ALE client implementation that
+ * contains the methods stub's for the ALE service
+ * of the Fosstrak Filtering & Collecting Server.
+ */
+class ALEClient {
+
+}
