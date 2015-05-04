a5f87e084a28d308575d50199adb597bcce74958
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-05-04 19:58:40 +0100

    Create the HALReader class.

---
+++ cloud4things-cli/src/main/scala/cloud4things/cli/utils/Reader.scala
@@ -0,0 +1,11 @@
+package cloud4things.cli.utils
+
+/* Reader: class that represents a RFID reader.
+   Params:
+    - @readerName : String = the reader name.
+    - @readerDescription : String = the reader description
+*/
+class Reader(name: String) {
+  val readerName : String = name
+  val readerDescription : String = "Description"
+}
