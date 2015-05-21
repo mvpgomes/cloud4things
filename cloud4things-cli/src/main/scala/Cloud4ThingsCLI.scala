39562f1b98fc730bc0def203e1b3a03c3395f4e4
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-05-21 16:44:05 +0100

    Create a configuration manager for the configuration of the smart-places.

--- cloud4things-cli/src/main/scala/Cloud4ThingsCLI.scala
+++ cloud4things-cli/src/main/scala/Cloud4ThingsCLI.scala
@@ -10,10 +10,7 @@
       each reader.
    ------------------------------------------------------- */
 
-import cloud4things.cli.configuration.Configuration
-import cloud4things.cli.eventcycle.EventCycle
 import cloud4things.cli.fosstrak.client.{FosstrakALEClient, FosstrakALELRClient}
-import cloud4things.cli.reader.Reader
 
 import scala.io.{Source, StdIn}
 
@@ -29,17 +26,6 @@ object Cloud4ThingsCLI {
 
   // Fosstrak ALELRClient instance
   private val ALELRClient = new FosstrakALELRClient()
-
-  // Smart-places configuration
-  private val smartConfiguration = new scala.collection.mutable.Map[String, Configuration] {
-    override def get(key: String): Option[Configuration] = ???
-
-    override def iterator: Iterator[(String, Configuration)] = ???
-
-    override def +=(kv: (String, Configuration)): this.type = ???
-
-    override def -=(key: String): this.type = ???
-  }
 
   // Print the options for the user choose
   def printOptions() {
@@ -66,34 +52,6 @@ object Cloud4ThingsCLI {
     print("Enter the ALELRService URL: ")
     ALELRClient.setAleLrServiceEndpointAddress(StdIn.readLine)
   }
-
-  // Create a new smart-place configuration
-  def createSmartPlaceConfiguration(name: String) = {
-    smartConfiguration += (name -> new Configuration(name))
-  }
-
-  // Add a reader to a configuration
-  def addReaderToConfiguration(configName: String, reader: Reader) = {
-    smartConfiguration(configName).addReader(reader)
-  }
-
-  //  Delete a reader from a configuration
-  def deleteReaderFromConfiguration(configName: String, readerName: String) = {
-    smartConfiguration(configName).deleteReader(readerName)
-  }
-
-  // Add event cycle to a configuration
-  def addEventCycleToConfiguration(configName: String, eventCycle: EventCycle) = {
-    smartConfiguration(configName).addEventCycle(eventCycle)
-  }
-
-  //  Delete a eventCycle from a configuration
-  def deleteEventFromConfiguration(configName: String, eventName: String) = {
-    smartConfiguration(configName).deleteEventCycle(eventName)
-  }
-
-  // Subscribe the EventCycles to the URL of the capturing application
-  def subscribeEventCycle(name: String, url: String) = {}
 
   /*
     - main : the main functions runs an interactive command-line
