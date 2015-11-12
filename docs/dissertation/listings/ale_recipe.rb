d356e9c71bfd71fc1497cdebef140ec5da957cc4
mvpgomes <mvp.gomes1989@gmail.com>
2015-11-12 15:34:24 +0000

    Add the missing provisioning recipes to the implementation closes #173.

--- docs/dissertation/listings/ale_recipe.rb
+++ docs/dissertation/listings/ale_recipe.rb
@@ -1,10 +1,10 @@
 # Pull latest image
 docker_image 'cloud4things/fosstrak_ale'
 
-# Run container exposing port 8080 and remaping to port 8080
+# Run container exposing port 8081 and remaping to port 8080
 docker_container 'cloud4things/fosstrak_ale' do
   detach true
   container_name 'fosstrak_ale'
   link 'fosstrak_capture:capture'
-  port ["8080:8080", "5556:5556"]
+  port "8081:8080"
 end
